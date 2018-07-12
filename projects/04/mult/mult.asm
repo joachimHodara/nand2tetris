48717bbeee3c25e13cc8108405229a5319e1b272
Joachim Hodara <joachim.hodara@gmail.com>
2018-07-12 00:06:31 -0400

    multiplicate function in assembly language

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2
+M=0     // R2 = 0
+
+@i
+M=1     // i = 0
+
+(LOOP)
+   @i
+   D=M
+   @R1
+   D=D-M
+   @END
+   D;JGT     // if i > R1 goto END
+
+   @R0
+   D=M
+   @R2
+   M=M+D     // R2 = R2 + R0
+
+   @i
+   M=M+1     // i = i + 1
+
+   @LOOP
+   0;JMP     // goto LOOP
+
+(END)
+   @END
+   0;JMP     // infinite loop
+
+
