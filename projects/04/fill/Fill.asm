3a9b1bf84ef60485fa1db7e91049987d9718525f
Joachim Hodara <joachim.hodara@gmail.com>
2018-07-13 22:18:28 -0400

    Filling exercice done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,51 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    @8192
+    D=A
+    @n
+    M=D      // n = 32*256 = 8192
+
+(KEYBOARD)
+
+    @i
+    M=0      // restart counter
+
+(LOOP)
+
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ    // goto WHITE if no key pressed
+
+(BLACK)
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1    // print black 
+    
+    @END
+    0;JMP   // skip white section
+
+(WHITE)
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0      // print white
+
+(END)
+
+    @i
+    MD=M+1   // i = i + 1
+    @n
+    D=D-M
+    @KEYBOARD
+    D;JEQ    // goto KBD if i == n
+
+    @LOOP
+    0;JMP
+
